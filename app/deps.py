12b10a5dda38e68bb43592baf95bf41ea7d706f0
Gabriel Vieira <gabrielvieira840@gmail.com>
2025-08-31 09:54:08 -0300

    fix: fixes deps creation bug

--- app/deps.py
+++ app/deps.py
@@ -14,8 +14,8 @@ def get_state() -> MongoState:
     return MongoState()
 
 @lru_cache(maxsize=1)
-def get_detector() -> Detector:
-    svc = get_related_service()
+async def get_detector() -> Detector:
+    svc = await get_related_service()
     all_items = asyncio.run(svc.list_relateds())
     all_classes = list({item["related"] for item in all_items["items"]})
 
@@ -24,15 +24,17 @@ def get_detector() -> Detector:
     return Detector(yolo_detector=yolo, clip_classifier=classifier, classes=all_classes)
 
 @lru_cache(maxsize=1)
-def get_related_service() -> RelatedService:
-    db = asyncio.run(get_db())
+async def get_related_service() -> RelatedService:
+    db = await get_db()
     return RelatedService(db)
 
 @lru_cache(maxsize=1)
-def get_item_service() -> ItemService:
-    db = asyncio.run(get_db())
+async def get_item_service() -> ItemService:
+    db = await get_db()
     return ItemService(db)
 
 @lru_cache(maxsize=1)
-def get_frame_service() -> FrameService:
-    return FrameService(get_detector(), get_state(), get_related_service())
+async def get_frame_service() -> FrameService:
+    detector = await get_detector()
+    related_service = await get_related_service()
+    return FrameService(detector, get_state(), related_service)
