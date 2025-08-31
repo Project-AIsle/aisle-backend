663c05a11ce368be91690787339e5213a15167fe
Gabriel Vieira <gabrielvieira840@gmail.com>
2025-08-31 10:12:32 -0300

    fix: removes async lru cache

--- app/deps.py
+++ app/deps.py
@@ -9,31 +9,26 @@ from .services.frame_service import FrameService
 from .config import settings
 import asyncio
 
-@lru_cache(maxsize=1)
 def get_state() -> MongoState:
     return MongoState()
 
-@lru_cache(maxsize=1)
 async def get_detector() -> Detector:
     svc = await get_related_service()
-    all_items = asyncio.run(svc.list_relateds())
+    all_items = await svc.list_relateds()
     all_classes = list({item["related"] for item in all_items["items"]})
 
     yolo = YOLOXDetector(model_path=settings.YOLOX_MODEL, providers=[settings.ORT_PROVIDERS], input_size=(settings.YOLOX_INPUT_W, settings.YOLOX_INPUT_H), score_thr=settings.YOLOX_CONF_THR, iou_thr=settings.YOLOX_IOU_THR)
     classifier = CLIPClassifier(model_name=settings.CLIP_MODEL_NAME, device=settings.CLIP_DEVICE)
     return Detector(yolo_detector=yolo, clip_classifier=classifier, classes=all_classes)
 
-@lru_cache(maxsize=1)
 async def get_related_service() -> RelatedService:
     db = await get_db()
     return RelatedService(db)
 
-@lru_cache(maxsize=1)
 async def get_item_service() -> ItemService:
     db = await get_db()
     return ItemService(db)
 
-@lru_cache(maxsize=1)
 async def get_frame_service() -> FrameService:
     detector = await get_detector()
     related_service = await get_related_service()
